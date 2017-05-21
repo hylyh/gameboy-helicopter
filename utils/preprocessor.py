6f2095e597624eebf9ed7503e5f946a55adfa2b9
Jessica Hayley <hey@jayhay.me>
2017-05-21 17:07:48 -0400

    Add a preprocessor that loads sprite data from images

--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -1,8 +1,58 @@
 import sys
 import re
 
+from PIL import Image
+
 DEFAULTFILE = '../helicopter.asm'
 MACROREGEX = r'([^ ]+) +{{ *(.+) *}}'
+
+def sprites(*args):
+    sprites = ''
+    for arg in args:
+        sprites += sprite(arg) + '\n'
+
+    return sprites
+
+def sprite(path):
+    img = Image.open('sprites/' + path + '.png')
+
+    bytes1, bytes2 = [], []
+
+    col = 0
+    for pixels in list(img.getdata()):
+        if col == 0:
+            bytes1.append([])
+            bytes2.append([])
+
+        r, g, b = pixels
+
+        # Only check the red channel bc lazy
+        if r == 255:
+            # White
+            bytes1[-1].append('0')
+            bytes2[-1].append('0')
+        elif r == 0:
+            # Black
+            bytes1[-1].append('1')
+            bytes2[-1].append('1')
+        elif r < 128:
+            # Dark
+            bytes1[-1].append('0')
+            bytes2[-1].append('1')
+        else:
+            # Light
+            bytes1[-1].append('1')
+            bytes2[-1].append('0')
+
+        col += 1
+        if col > 7:
+            col = 0
+
+    output = ''
+    for byte1, byte2 in zip(bytes1, bytes2):
+        output += 'DB %' + ''.join(byte1) + ',%' + ''.join(byte2) + '\n'
+
+    return output
 
 # Just echos back what you put in, for testing
 def echo(thing):
@@ -17,6 +67,8 @@ def process(inname, outname=None):
             if match:
                 label = match.group(1)
                 expression = match.group(2)
+
+                # Eval whatever they put in the macro
                 result = eval(expression)
 
                 # Rebuild the line with the new result
