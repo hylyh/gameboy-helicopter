2c72408f014677290b979e45169a6828bc6f286b
Jessica Hayley <hey@jayhay.me>
2017-05-21 11:36:58 -0400

    Fix a preprocessor issue

--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -24,14 +24,14 @@ def process(inname, outname=None):
 
             output_lines.extend(new_line or [line])
 
-    with open(outname, 'w') as f:
+    with open(outname or inname + '.built', 'w') as f:
         f.write(''.join(output_lines))
 
 def main():
     if len(sys.argv) != 2 or len(sys.argv != 3):
         print('Please provide a single argument pointing to the .asm you want to process')
         print('Optionally you may provide a second argument specifiying the output file name')
-        print('Assuming "' +  '"for testing purposes')
+        print('Assuming "' + DEFAULTFILE + '" for testing purposes')
 
     process(sys.argv[1] if len(sys.argv) > 1 else DEFAULTFILE, sys.argv[2] if len(sys.argv) > 2 else None)
 
